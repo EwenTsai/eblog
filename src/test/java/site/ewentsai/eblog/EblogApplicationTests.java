bb25642b84ab40c7e19cb69ca3e19619cae37119
ewentsai <ewen.chua.gi@gmail.com>
2020-04-14 19:40:50 +0800

    Initial commit

---
+++ src/test/java/site/ewentsai/eblog/EblogApplicationTests.java
@@ -0,0 +1,13 @@
+package site.ewentsai.eblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
